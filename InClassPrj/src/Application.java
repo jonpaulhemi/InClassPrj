b8a6e5b3f25273c7f296f5d3daa2e3a3026123ac
jonpaulhemi <jonpaulhemi@gmail.com>
2015-03-02 15:08:42 -0500

    No

--- InClassPrj/src/Application.java
+++ InClassPrj/src/Application.java
@@ -1,23 +1,14 @@
 
 public class Application {
 
-	public static void main(String[] args) {
-		int counter = 0;
-		boolean add = true;
+	public static void main(String[] args) throws InterruptedException {
+		char[] nickSucks = {'N', 'I', 'C', 'K', ' ', 'S', 'U', 'C', 'K', 'S'};
 		while (true) {
-			
-			if (counter > 10)
-				add = false;
-			if(add)
-				counter++;
-			else 
-				counter--;
-			
-			for (int i = 0; i<=counter; i++) {
-				System.out.print(" ");
+			for (int i = 0; i < nickSucks.length; i++) {
+				System.out.print(nickSucks[i]);
+				Thread.sleep(200);
 			}
-		System.out.println("*");
-		
+			System.out.println();
 		}	
 	}
 	
